73467e4aeb7b94d58eade22d3c917284a2221053
zhamdana <zaharadeenhamdana@gmail.com>
2026-02-23 07:43:17 +0100

    Correction

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss(),],
   base: '/travel-landing-page/',
 })
