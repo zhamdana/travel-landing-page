53c5562d49544fab1ac8d9669dbe49563b8ef9b7
zhamdana <zaharadeenhamdana@gmail.com>
2026-02-23 07:37:33 +0100

    Correction

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './components/Navbar.jsx'
+
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Testimonials from './components/Testimonials.jsx'
